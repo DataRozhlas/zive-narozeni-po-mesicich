3b89d41ad7ede209ff91a2e6241a56dc3c8b8fa4
Petr Kočí <petr.koci@samizdat.cz>
2020-03-26 16:18:21 +0100

    graf

--- getData.R
+++ getData.R
@@ -1,6 +1,7 @@
 library(readxl)
 library(tidyr)
 library(lubridate)
+library(jsonlite)
 
 data <- read_excel("130055190609.xlsx", skip=4, col_names = c("rok", "celkem", c(1:12)))
 
@@ -11,3 +12,5 @@ data <- pivot_longer(data, -rok, names_to = "month", values_to = "y")
 data$datum <- paste0(data$rok, "-", formatC(as.numeric(data$month), width=2, format="d", flag="0"), "-01")
 
 data$x <- as.numeric(as_date(data$datum)) * 86400000
+
+toJSON(data[,c(5,3)])
